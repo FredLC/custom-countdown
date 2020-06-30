050f9ad94025b3bbe90ad4ef7210220bbcc490e6
fredlc <fredlefevre@outlook.com>
2020-06-29 20:16:39 -0400

    Integrated localStorage

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -56,6 +57,11 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate,
+    };
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown));
     if (countdownDate === '') {
         alert('Please select a date.');
     } else {
@@ -71,8 +77,22 @@ function reset() {
     clearInterval(countdownActive);
     countdownTitle = '';
     countdownDate = '';
+    localStorage.removeItem('countdown');
+}
+
+function restorePreviousCountdown() {
+    if (localStorage.getItem('countdown')) {
+        inputContainer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
 }
 
 countdownForm.addEventListener('submit', updateCountdown);
 countdownButton.addEventListener('click', reset);
 completeElButton.addEventListener('click', reset);
+
+restorePreviousCountdown();
